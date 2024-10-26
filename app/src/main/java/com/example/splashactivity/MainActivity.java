1d371ddf028ea753f53818bb5244be72ac228751
SeemaTarghale <seematarghale26@gmail.com>
2024-10-26 22:49:32 +0530

    Initial commit

---
+++ app/src/main/java/com/example/splashactivity/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.splashactivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        TextView t1;
+        EditText e1,e2;
+        Button b1;
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        t1=findViewById(R.id.txt1);
+        e1=findViewById(R.id.ed1);
+        e2=findViewById(R.id.ed2);
+        b1=findViewById(R.id.btn);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(i);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/splashactivity/SplashActivity.java
@@ -0,0 +1,56 @@
+package com.example.splashactivity;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashActivity extends AppCompatActivity {
+
+
+    @SuppressLint("MissingInflatedId")
+    @Override  //Annotation
+    protected void onCreate(Bundle savedInstanceState) {
+        ImageView image;
+        TextView text;
+        Animation fadeInAnim;
+        super.onCreate(savedInstanceState);
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+
+        setContentView(R.layout.activity_splash);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        image=findViewById(R.id.logo);
+        text=findViewById(R.id.text);
+        fadeInAnim= AnimationUtils.loadAnimation(SplashActivity.this,R.anim.animation);
+        image.setAnimation(fadeInAnim);
+        text.setAnimation(fadeInAnim);
+        Handler h;  //object declaration
+        h=new Handler(); // object Initialization
+        //Handler h=new Handler();     Shortcut
+        h.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i=new Intent(SplashActivity.this,MainActivity.class);   //Alt+Enter
+                startActivity(i);
+                finish();
+            }
+        },4000);
+
+
+    }
+
+}
